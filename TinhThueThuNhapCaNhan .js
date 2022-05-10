397345f24443fde27ae08f246c4ce540af96f378
nsonhai1.1 <nsonhai1.1@gmail.com>
2022-05-10 09:50:51 +0700

    create project

---
+++ TinhThueThuNhapCaNhan .js
@@ -0,0 +1,5 @@
+function getTaxableIncome() {
+	var fullName = document.getElementById("fullName").value
+	var totalIncome = document.getElementById("totalIncome").value
+	var amountPeople = document.getElementById("amountPeople").value
+}
